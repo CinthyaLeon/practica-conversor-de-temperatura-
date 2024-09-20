7c5bbadd6ea6b4f56eb326cdc165462ba5b50c87
Cinthya León <162649790+CinthyaLeon@users.noreply.github.com>
2024-09-19 20:47:47 -0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("conversor de temperaturas");
+        double celsius = 1.8;
+        double fahrenheit = (celsius * 1.8) + 32;
+        int temperaturaCompleta = (int) fahrenheit;
+        String mensaje = """
+                La temperatura grados celsius es: %.1f
+                La temperatura equivalente en grados fahrenheit es: %.1f
+                """.formatted(celsius,fahrenheit );
+        System.out.println(mensaje);
+        System.out.printf(String.format("la temperaturaCompleta es: %f%n", fahrenheit ));
+
     }
-}
+    }
